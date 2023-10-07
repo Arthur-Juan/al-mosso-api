aa4516b5f016ab9ff3200c6ee05a1745a0423d58
Arthur Lima <arthur.lima@inforce.com.br>
2023-10-07 18:46:06 -0300

    feat: exposing uploads folder to public

--- internal/router/router.go
+++ internal/router/router.go
@@ -4,6 +4,7 @@ import "github.com/gofiber/fiber/v2"
 
 func Initialize() {
 	router := fiber.New()
+	router.Static("/uploads", "./uploads")
 	startRoutes(router)
 	err := router.Listen(":8080")
 	if err != nil {
